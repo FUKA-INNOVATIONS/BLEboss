79ed9679a2750d1a774d8b337e921467d992c293
Fuwad Kalhori <fuad.kalhori@gmail.com>
2022-12-30 09:13:18 +0200

    added runtime permission request to Context as extension functions

--- app/src/main/java/com/fuka/bleboss/MainActivity.kt
+++ app/src/main/java/com/fuka/bleboss/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.fuka.bleboss
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,10 +17,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.fuka.bleboss.ui.theme.BLEbossTheme
 
 // Bluetooth-enabling action we’ll soon request from the user. It can be any positive integer value
 private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+private const val RUNTIME_PERMISSION_REQUEST_CODE = 2
 
 class MainActivity : ComponentActivity() {
 
@@ -55,6 +60,31 @@ class MainActivity : ComponentActivity() {
             startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
         }
     }
+
+
+
+    /*
+    * The two functions above are extension functions on the Context object,
+    * meaning each Context instance with access to these functions can call them and use them as
+    * if these functions were part of the original class declaration. Depending on how you structure your source code,
+    * these extension functions may reside in another Kotlin source file so that they can be accessed by other Activities as well.
+    * */
+
+    // Request runtime permission from the user: in your Activity, ViewModel, or at least in a Context
+
+    fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun Context.hasRequiredRuntimePermissions(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            hasPermission(Manifest.permission.BLUETOOTH_SCAN) && hasPermission(Manifest.permission.BLUETOOTH_CONNECT)
+        } else {
+            hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+
 
 }
 
