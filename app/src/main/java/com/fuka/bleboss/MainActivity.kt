7a556a59689acf7e30dca4b2c62ac1eb14739c40
Fuwad Kalhori <fuad.kalhori@gmail.com>
2022-12-30 09:04:43 +0200

    added activate bluetooth repquest prompt

--- app/src/main/java/com/fuka/bleboss/MainActivity.kt
+++ app/src/main/java/com/fuka/bleboss/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.fuka.bleboss
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,7 +16,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fuka.bleboss.ui.theme.BLEbossTheme
 
+// Bluetooth-enabling action we’ll soon request from the user. It can be any positive integer value
+private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+
 class MainActivity : ComponentActivity() {
+
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -27,6 +40,22 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        if (!bluetoothAdapter.isEnabled) {
+            promptEnableBluetooth()
+        }
+    }
+
+    private fun promptEnableBluetooth() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
+        }
+    }
+
 }
 
 @Composable
